a3c01ff1aea5f6d638aaf1b24c0ba88313400670
Pedro Miclos <pedromcls74@gmail.com>
2026-06-04 12:54:21 -0300

    configuracao para deploy no vercel

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 
 @app.route("/")
@@ -31,5 +31,4 @@ def resultado():
     )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+handler = app
