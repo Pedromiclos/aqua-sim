c8425eef68984df3eebcffc44b5c755ff06fddf6
Pedro Miclos <pedromcls74@gmail.com>
2026-06-04 12:58:40 -0300

    corrige css no vercel

--- api/app.py
+++ api/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
-
+app = Flask(
+    __name__,
+    template_folder="../templates",
+    static_folder="../static",
+    static_url_path="/static"
+)
 
 @app.route("/")
 def index():
